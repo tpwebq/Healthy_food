0e4545f8364a985028eece0f8f51e7e15db7e0f9
TP <pankowski.tomasz@prokonto.pl>
2022-10-13 22:38:13 +0100

    layout

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutRoutingModule } from './about-routing.module';
+import { AboutS1Component } from './about-s1/about-s1.component';
+import { AboutS2Component } from './about-s2/about-s2.component';
+import { AboutS3Component } from './about-s3/about-s3.component';
+import { AboutComponent } from './about.component';
+
+
+@NgModule({
+  declarations: [
+    AboutS1Component,
+    AboutS2Component,
+    AboutS3Component,
+    AboutComponent
+  ],
+  imports: [
+    CommonModule,
+    AboutRoutingModule
+  ]
+})
+export class AboutModule { }
